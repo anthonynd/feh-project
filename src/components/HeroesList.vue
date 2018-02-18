2bb490ba095f3d58333c3adfe6e1cde7c3352c45
Anthony Nguyen-Duong <anthnd@hotmail.com>
2018-02-18 16:22:22 -0500

    init

---
+++ src/components/HeroesList.vue
@@ -0,0 +1,35 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: 'HeroesList',
+    data() {
+      return {
+        heroes: [
+          {
+            name: 'Anna',
+            rarity: 4,
+            level: 40,
+            stats: {
+              hp: 38,
+              attack: 27,
+              speed: 36,
+              defense: 20,
+              resisitance: 25
+            },
+            weapon: {
+              name: 'Silver Axe',
+              might: 11
+            },
+            skills: {
+              a: null,
+              b: 'Vantage 2',
+              c: 'Spur Res 3'
+            }
+          }
+        ]
+      }
+    }
+  }
+</script>
